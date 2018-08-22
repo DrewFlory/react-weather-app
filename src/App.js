d74f8527cb43bf5b781c44c6927ad974afe025bc
John Doe <ajflory9@yahoo.com>
2018-08-22 00:59:57 -0400

    weather-api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import Titles from "./components/Title"
+import Titles from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+
+const API_KEY = "8130cd9aa5353512b52150f82bbe94f6";
+
 
 class App extends React.Component {
-    render(){
+
+    getWeather = async () => {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?
+            id=524901&APPID=${API_KEY}`);
+    }
+       render(){
         return (
             <div>
                 <Titles />
+                <Form />
+                <Weather />
             </div>
         );
     }
